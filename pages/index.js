9587d668ecadc41dcc971dfd65ce9bc90f187cfc
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-23 17:34:02 +0900

    implement a hero image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,8 +16,23 @@ export default function Home() {
 
       <section className="bg-gray-100 pb-10 px-10 ">
         <div className="max-w-3xl mx-auto">
-          <div className="py-6">
-            <h2 className="text-gray-700">Start a new document</h2>
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              ripple="dark"
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-pointer">
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+            <p>Blank</p>
           </div>
         </div>
       </section>
