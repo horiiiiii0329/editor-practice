b7bc289641c80c7daa4b8c3235f6380a181f2783
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-23 16:28:32 +0900

    initial setup

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+function Header() {
+  return <div className="flex items-center "></div>;
+}
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Hoogle Docs Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+    </div>
+  );
+}
