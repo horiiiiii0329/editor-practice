745db7ebc3248edad103ddaf73754ff1a08413a9
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-23 17:54:07 +0900

    complete  main header

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
             </Button>
           </div>
           <div>
-            <div className="relative h-52 w-40 border-2 cursor-pointer cursor-pointer hover:border-blue-700">
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
               <Image src="https://links.papareact.com/pju" layout="fill" />
             </div>
             <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
@@ -38,7 +38,15 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section></section>
+      <section className="bg-white px-10 md:px-0">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray">
+          <div className="flex items-center justify-between pb-5 text-sm text-gray-700">
+            <h2 className="font-medium flex-grow">My Document</h2>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
