04c2efffed2e7de4aa3c436cb37bb55e6b0ad73a
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-26 22:32:27 +0900

    get an id

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -9,15 +9,15 @@ import { getDoc } from "firebase/firestore";
 
 function Doc() {
   const { data: session, status } = useSession();
-  //   const data = getDoc(db, "userDoc", id);
   const router = useRouter();
   const { id } = router.query;
 
   if (status === "unauthenticated") return <Login />;
+
   return (
     <div>
       <header className="flex justify-between items-center p-3 pb-1">
-        <span onClick={() => router.push("/")} classsName="cursor-pointer">
+        <span onClick={() => router.push("/")} className="cursor-pointer">
           <Icon name="description" size="5xl" color="blue" />
         </span>
         <div>
