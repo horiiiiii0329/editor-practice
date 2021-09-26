d87473312a6c78798d8f33d061d6bf62182c9a97
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-26 23:14:36 +0900

    styling nv of note

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -3,16 +3,24 @@ import Icon from "@material-tailwind/react/Icon";
 import { useRouter } from "next/dist/client/router";
 import { db } from "../../firebase";
 import { getSession, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 import Login from "../../components/Login";
 import { router } from "../../.next/static/chunks/main";
-import { getDoc } from "firebase/firestore";
+import { getDoc, doc } from "firebase/firestore";
 
 function Doc() {
+  const [note, setNote] = useState([]);
   const { data: session, status } = useSession();
   const router = useRouter();
   const { id } = router.query;
+  //   const docRef = doc(db, "userDoc", id);
+  //   const docSnap = getDoc(docRef);
 
   if (status === "unauthenticated") return <Login />;
+
+  //   if(!loadingSnapshot && !snapshot?.data()?.fileName){
+  //       router.replace("/")
+  //   }
 
   return (
     <div>
@@ -20,8 +28,16 @@ function Doc() {
         <span onClick={() => router.push("/")} className="cursor-pointer">
           <Icon name="description" size="5xl" color="blue" />
         </span>
-        <div>
-          <h2></h2>
+        <div className="flex-grow px-2">
+          <h2>DUMMY TITLE</h2>
+          <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
+            <p className="option">File</p>
+            <p className="option">Edit</p>
+            <p className="option">View</p>
+            <p className="option">Insert</p>
+            <p className="option">Format</p>
+            <p className="option">Tools</p>
+          </div>
         </div>
       </header>
     </div>
