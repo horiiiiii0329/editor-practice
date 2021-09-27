1cec61b83b48364e87603b580d5cf7405ffbee6a
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-28 00:02:48 +0900

    change a draft.js

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,5 +1,9 @@
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import dynamic from "next/dynamic";
+import { EditorState } from "draft-js";
+import { useState, useRef } from "react";
+import { db } from "../firebase";
+import { collection, doc } from "@firebase/firestore";
 
 const Editor = dynamic(
   () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
@@ -7,9 +11,19 @@ const Editor = dynamic(
 );
 
 function TextEditor() {
+  const { editorState, setEditorState } = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
   return (
     <div className=" min-h-screen pb-16">
       <Editor
+        editorState={editorState}
+        onEditorStateChange={onEditorStateChange}
         toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
         editorClassName="mt-6 p-10 bg-white shadow-lg max-w-6xl mx-auto !h-screen"
       />
