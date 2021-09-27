f341c9d0bfb5632b4cbf2fe5cd7201820645f28c
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-27 22:20:33 +0900

    change db

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -3,9 +3,9 @@ import Icon from "@material-tailwind/react/Icon";
 import { useRouter } from "next/dist/client/router";
 import { db } from "../../firebase";
 import { getSession, signOut, useSession } from "next-auth/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "../../components/Login";
-import { getDoc, doc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import Button from "@material-tailwind/react/Button";
 import TextEditor from "../../components/TextEditor";
 
@@ -13,14 +13,28 @@ function Doc() {
   const { data: session, status } = useSession();
   const router = useRouter();
   const { id } = router.query;
-  //   const docRef = doc(db, "userDoc", id);
-  //   const docSnap = getDoc(docRef);
+  const [snapShot, setSnapShot] = useState([]);
 
   if (status === "unauthenticated") return <Login />;
 
-  //   if(!loadingSnapshot && !snapshot?.data()?.fileName){
-  //       router.replace("/")
-  //   }
+  useEffect(() => {
+    let docs = [];
+    getDocs(collection(db, "userDoc"))
+      .then((snapshot) => {
+        snapshot.docs.forEach((note) => {
+          let currentID = note.id;
+          let appObj = { ...note.data(), ["id"]: currentID };
+          docs.push(appObj);
+        });
+      })
+      .then(console.log(docs.map((doc) => doc.fieldName)));
+
+    setSnapShot(docs);
+  }, []);
+
+  // if (snapShot) {
+  //   setSnapShot(snapShot.filter((item) => item.id === id));
+  // }
 
   return (
     <div>
@@ -29,7 +43,7 @@ function Doc() {
           <Icon name="description" size="5xl" color="blue" />
         </span>
         <div className="flex-grow px-2">
-          <h2>DUMMY TITLE</h2>
+          <h2>{snapShot?.fieldName}</h2>
           <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-600">
             <p className="option">File</p>
             <p className="option">Edit</p>
@@ -54,7 +68,7 @@ function Doc() {
         </Button>
         <img
           className="rounded-full cursor-pointer h-10 w-10 ml-2"
-          src={session.user.image}
+          src={session?.user?.image}
           alt=""
         />
       </header>
